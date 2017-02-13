1841c6cd77fff6da48a67bf739c116d34ef2a88b
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-02-13 18:59:34 +0100

    ref #27: Add section on smart_handle
    Wrote a section regarding the smart_handle structure.

---
+++ design_document/code_samples/smart_handle.cpp
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////
+/// Look at function definitions on cppreference.com, as these
+/// functions are not entirely correct.
+/// the interface should mimic the std::unique_ptr interface,
+/// regarding dereferencing it etc.
+////////////////////////////////////////////////////////////////
+template<class T>
+class smart_handle
+{
+public:
+    ////////////////////////////////////////////////////////////
+    /// \brief Default constructor. 
+    ///        Handle is not valid unless constructed 
+    ///        with parameters.
+    ////////////////////////////////////////////////////////////
+    smart_handle() = default;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Proper constructor, the only one that leaves
+    ///        a smart handle in valid form.
+    ///
+    /// \param object The object to reference.
+    /// \param generation The generation the reference 
+    ///        was created.
+    /// \param meta_information Pointer the meta_information
+    ///        of the component type.
+    ////////////////////////////////////////////////////////////
+    smart_handle(T* object, 
+                 std::size_t generation, 
+                 meta_information* meta_info);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Gets the underlying pointer to the object.
+    ///        Access is constant if not pointer is not
+    ///        invalidated.
+    ///
+    /// \return Pointer to the object. 
+    ///         nullptr if object was not found.
+    ////////////////////////////////////////////////////////////
+    T* get() const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Convenience function for get.
+    ///
+    /// \return Pointer to the object.
+    ///
+    /// \see get
+    ////////////////////////////////////////////////////////////
+    T* operator*() const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Convenience function for get.
+    ///
+    /// \return Pointer to the object.
+    ///
+    /// \see get
+    ////////////////////////////////////////////////////////////
+    T* operator->() const;
+};
