43b8bf72eefe0b03cff2f40eacbba0456583f4a0
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-02-14 18:10:08 +0100

    ref #27: Add section on meta_information
    Added a section on meta_informationm, it is not entirely complete yet
    though.

---
+++ design_document/code_samples/meta_information.cpp
@@ -0,0 +1,104 @@
+struct MetaInformation
+{
+    ////////////////////////////////////////////////////////////
+    /// \brief Used to identify the type of the components
+    ///        belonging to this MetaInformation.
+    ////////////////////////////////////////////////////////////
+    TypeIdentifier typeIdentifier;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Contains the size of the object in bytes.
+    ///        Used for memory allocation, and potentially
+    ///        pointer arithmetic.
+    ////////////////////////////////////////////////////////////
+    std::size_t sizeInBytes;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Used for construction if the class in question.
+    ///
+    /// \detail This defaults to the correct constructor of 
+    ///         the class with in-place construction.
+    ///         Constructor must be parameterless.
+    ///         Should not be overloaded in most cases.
+    ////////////////////////////////////////////////////////////
+    SingleOperation constructor;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Used for destruction if the class in question.
+    ///
+    /// \detail This defaults to the correct destruction of 
+    ///         the class with in-place construction.
+    ///         Constructor must be parameterless.
+    ///         Should not be overloaded in most cases.
+    ////////////////////////////////////////////////////////////
+    SingleOperation destructor;
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief Called on a newly created object.
+    ///        Created objects are not updated before they are
+    ///        activated.
+    ////////////////////////////////////////////////////////////
+    SingleOperation onAwake;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Called when an object is requested to 
+    ///        be activated.
+    ///        See lifecycle section.
+    ////////////////////////////////////////////////////////////
+    SingleOperation onActivate;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Called when an object is requested to be
+    ///        deactivated.
+    ///        See lifecycle section.
+    ////////////////////////////////////////////////////////////
+    SingleOperation onDeactivate;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Called when an object is going to be destroyed.
+    ///        Only deactivated objects can be destroyed.
+    ///        
+    ////////////////////////////////////////////////////////////
+    SingleOperation onHibernate;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief The update function that will be on each
+    ///        component type once per frame.
+    ////////////////////////////////////////////////////////////
+    RangedOperation onUpdate;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief What sort of data access does the update
+    ///        function do?
+    ///        See multi-threading section.
+    ////////////////////////////////////////////////////////////
+    DataAccess onUpdateDataAccess;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief The function that will be run on each component
+    ///        type per event from other parts of the engine.
+    ///        i.e. The NOX Event system.
+    ////////////////////////////////////////////////////////////
+    RangedOperation onLogicEvent;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief What sort of data access does the onLogicEvent
+    ///        function do.
+    ///        See multi-threading section.
+    ////////////////////////////////////////////////////////////
+    DataAccess onLogicEventDataAccess;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief This function is called on components when an
+    ///        ActorEvent targeted them occurs.
+    ///        This is with the "new" 
+    ////////////////////////////////////////////////////////////
+    RangedOperation onActorEvent;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief What sort of data access does the onActorEvent
+    ///        function do.
+    ///        See multi-threading section.
+    ////////////////////////////////////////////////////////////
+    DataAccess onActorEventDataAccess;
+};
