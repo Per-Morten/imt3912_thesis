cc9c7554a3ae3a815312e02b0ffa0646c516ad27
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-02-14 17:45:39 +0100

    ref #27: Add doxygen comments to type_identifier
    Added doxygen style comments to the type_identifier for consistency,
    also added two convencience overloads.

--- design_document/code_samples/type_identifier.cpp
+++ design_document/code_samples/type_identifier.cpp
@@ -1,8 +1,39 @@
 class TypeIdentifier
 {
 public:
+    ////////////////////////////////////////////////////////////
+    /// \brief Creates a TypeIdentifier with a given numerical
+    ///        value, this value is not hashed!
+    ///        User can pre_hash if wanted.
+    ///
+    /// \param identifier The numerical value used to identify
+    ///                   this type.
+    ////////////////////////////////////////////////////////////
     TypeIdentifier(std::size_t identifier);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Creates a TypeIdentifier with a numerical value
+    ///        based on std::hash<std::string>.
+    ///
+    /// \param identifier The string used to create a 
+    ///                   hash value identifying this type.
+    ////////////////////////////////////////////////////////////
     TypeIdentifier(const std::string& identifier);
 
+    ////////////////////////////////////////////////////////////
+    /// \brief Returns the underlying numerical value.
+    ////////////////////////////////////////////////////////////
     std::size_t value() const;
 };
+
+////////////////////////////////////////////////////////////////
+/// \brief Convenience overload allowing for easy comparing.
+////////////////////////////////////////////////////////////////
+bool operator==(const TypeIdentifier& lhs, 
+                const TypeIdentifier& rhs);
+
+////////////////////////////////////////////////////////////////
+/// \brief Convenience overload allowing for easy comparing.
+////////////////////////////////////////////////////////////////
+bool operator!=(const TypeIdentifier& lhs, 
+                const TypeIdentifier& rhs);
