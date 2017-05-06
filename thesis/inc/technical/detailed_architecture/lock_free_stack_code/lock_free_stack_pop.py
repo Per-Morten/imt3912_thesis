dec2dbfd06cb734fa782f5a27cee1d1133de014c
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-05-06 15:12:51 +0200

    ref #85: Add clarification to lock_free_stack
    Added a clarification to the lock free stack, regarding the poping
    semantics.
    Also added a short paragraph on the use of third party software.

--- thesis/inc/technical/detailed_architecture/lock_free_stack_code/lock_free_stack_pop.py
+++ thesis/inc/technical/detailed_architecture/lock_free_stack_code/lock_free_stack_pop.py
@@ -7,10 +7,13 @@ try_pop(out_value&): # 1
         ; # empty body
 
     out_value = expected.value
-    ~node(expected)
-    allocator.deallocate(expected)
+    ~node(expected) # 2
 
     return true
 
 # 1: Value is sent as an out parameter, to make it easier
 #    to use the try_pop function in a loop.
+# 2: Note that we are not deallocating here, just destroying
+#    the node.
+#    The stack will have its memory properly deallocated
+#    when the clear function is called.
